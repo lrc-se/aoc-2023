6b3f9a8db4df1db89c513b9738427166980d2add
Kalle Brisland <kalle.brisland@cygni.se>
2023-12-07 13:03:23 +0100

    Day 7: improved performance

--- day07/Cards.cs
+++ day07/Cards.cs
@@ -2,8 +2,8 @@ internal enum HandType { HighCard, OnePair, TwoPair, ThreeOfAKind, FullHouse, Fo
 
 internal readonly struct Card(char label, bool useJokers)
 {
-    public char Label { get; } = label;
-    public int Strength { get; } = label switch
+    public readonly char Label = label;
+    public readonly int Strength = label switch
     {
         'A' => 14,
         'K' => 13,
@@ -16,9 +16,9 @@ internal readonly struct Card(char label, bool useJokers)
 
 internal readonly struct Hand(Card[] cards, int bid, bool useJokers) : IComparable<Hand>
 {
-    public HandType Type { get; } = useJokers ? ComputeTypeWithJokers(cards) : ComputeType(cards.Select(card => card.Label));
-    public Card[] Cards { get; } = cards;
-    public int Bid { get; } = bid;
+    public readonly HandType Type = useJokers ? ComputeTypeWithJokers(cards) : ComputeType(cards.Select(card => card.Label).OrderBy(label => label).ToArray());
+    public readonly Card[] Cards = cards;
+    public readonly int Bid = bid;
 
     public int CompareTo(Hand other)
     {
@@ -38,9 +38,8 @@ internal readonly struct Hand(Card[] cards, int bid, bool useJokers) : IComparab
 
     private static readonly char[] _jokerLabels = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
 
-    private static HandType ComputeType(IEnumerable<char> labels)
+    private static HandType ComputeType(ReadOnlySpan<char> sortedLabels)
     {
-        var sortedLabels = labels.OrderBy(label => label).ToArray();
         List<int> runCounts = [];
         int runCount = 1;
         for (int i = 1; i < sortedLabels.Length; ++i)
@@ -58,14 +57,12 @@ internal readonly struct Hand(Card[] cards, int bid, bool useJokers) : IComparab
         if (runCount > 1)
             runCounts.Add(runCount);
 
-        runCounts.Sort();
-
         return runCounts switch
         {
             [2] => HandType.OnePair,
             [2, 2] => HandType.TwoPair,
             [3] => HandType.ThreeOfAKind,
-            [2, 3] => HandType.FullHouse,
+            [2, 3] or [3, 2] => HandType.FullHouse,
             [4] => HandType.FourOfAKind,
             [5] => HandType.FiveOfAKind,
             _ => HandType.HighCard
@@ -75,24 +72,28 @@ internal readonly struct Hand(Card[] cards, int bid, bool useJokers) : IComparab
     private static HandType ComputeTypeWithJokers(Card[] cards)
     {
         var bestType = HandType.HighCard;
+        var cache = new Dictionary<string, HandType>();
         var cardLabels = cards.Select(card => card.Label == 'J' ? _jokerLabels : [card.Label]).ToArray();
-        var labels = new char[5];
         for (int i0 = 0; i0 < cardLabels[0].Length; ++i0)
         {
-            labels[0] = cardLabels[0][i0];
             for (int i1 = 0; i1 < cardLabels[1].Length; ++i1)
             {
-                labels[1] = cardLabels[1][i1];
                 for (int i2 = 0; i2 < cardLabels[2].Length; ++i2)
                 {
-                    labels[2] = cardLabels[2][i2];
                     for (int i3 = 0; i3 < cardLabels[3].Length; ++i3)
                     {
-                        labels[3] = cardLabels[3][i3];
                         for (int i4 = 0; i4 < cardLabels[4].Length; ++i4)
                         {
-                            labels[4] = cardLabels[4][i4];
-                            var type = ComputeType(labels);
+                            var labels = new[] { cardLabels[0][i0], cardLabels[1][i1], cardLabels[2][i2], cardLabels[3][i3], cardLabels[4][i4] }.AsSpan();
+                            labels.Sort();
+
+                            string key = labels.ToString();
+                            if (!cache.TryGetValue(key, out var type))
+                            {
+                                type = ComputeType(labels);
+                                cache[key] = type;
+                            }
+
                             if (type > bestType)
                                 bestType = type;
                         }
